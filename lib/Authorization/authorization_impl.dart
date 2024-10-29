94b95c202d51c76b5dbdd96d1559a00bd3c5f94e
PhilopateerMaged02 <philopater4102@gmail.com>
2024-10-30 00:22:42 +0300

    LatestUpdates30

--- lib/Authorization/authorization_impl.dart
+++ lib/Authorization/authorization_impl.dart
@@ -1,9 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:fruit_app/Authorization/authorization.dart';
 import 'package:fruit_app/Failures/errors.dart';
+import 'package:fruit_app/Failures/exceptions.dart';
 import 'package:fruit_app/Service/FirebaseAuthService.dart';
 import 'package:fruit_app/models/UserEntity/user_entity.dart';
-import 'package:fruit_app/models/UserEntity/user_model.dart';
 import 'package:fruit_app/models/UserModel/user_model.dart';
 
 class AuthorizationImpl extends Authorization {
@@ -11,11 +11,17 @@ class AuthorizationImpl extends Authorization {
   AuthorizationImpl({required this.firebaseauthservice});
   @override
   Future<Either<Errors, UserEntity>> createUser(
-      String email, String password, String name) async {
-    var user =
-        await firebaseauthservice.createUser(email: email, Password: password);
-    return right(
-      UserModel.fromFirebaseUser(user),
-    );
+      String email, String password) async {
+    try {
+      var user = await firebaseauthservice.createUser(
+          email: email, Password: password);
+      return right(
+        UserModel.fromFirebaseUser(user),
+      );
+    } on CustomExceptions catch (e) {
+      return left(ServerFailure(e.message));
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
---
+++ lib/Cubits/CreateUserCubit/create_user_cubit.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_app/Authorization/authorization.dart';
+import 'package:fruit_app/Cubits/CreateUserCubit/create_user_states.dart';
+
+abstract class CreateUserCubit extends Cubit<CreateUserStates> {
+  CreateUserCubit(this.authorization) : super(CreateUserStateInitial());
+  final Authorization authorization;
+  Future<void> createUser(String email, String password) async {
+    emit(CreateUserStateLoading());
+    final result = await authorization.createUser(email, password);
+    result.fold(
+      (l) => emit(CreateUserStateError(message: l.error)),
+      (r) => emit(CreateUserStateSuccess(userEntity: r)),
+    );
+  }
+}
