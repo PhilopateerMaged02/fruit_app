f3fab22fbdb69a5970715e40f5366e86d69218de
PhilopateerMaged02 <philopater4102@gmail.com>
2024-10-30 15:47:30 +0300

    30\10

--- lib/Cubits/CreateUserCubit/create_user_cubit.dart
+++ lib/Cubits/CreateUserCubit/create_user_cubit.dart
@@ -2,7 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_app/Authorization/authorization.dart';
 import 'package:fruit_app/Cubits/CreateUserCubit/create_user_states.dart';
 
-abstract class CreateUserCubit extends Cubit<CreateUserStates> {
+class CreateUserCubit extends Cubit<CreateUserStates> {
   CreateUserCubit(this.authorization) : super(CreateUserStateInitial());
   final Authorization authorization;
   Future<void> createUser(String email, String password) async {
